dc2d4ac4028ef7f1bf017ca123e193103b840304
Henry Lam <hcylam@connect.ust.hk>
2023-10-26 19:40:30 +0800

    cleaned up the code, added debugging messages to see where the server is crashing

--- server_variant/snake_game.py
+++ server_variant/snake_game.py
@@ -1,6 +1,5 @@
 import time
 
-#from messages import messages
 from snake import snake
 from apple import apple
 from tiles import tiles
@@ -11,10 +10,7 @@ class snakegame:
         self.update_time = update_time
         self.width = 550
         self.height = 550
-        self.window_width = 650
-        self.window_height = 650
         self.apple_count = 0
-        self.buffer = ["none", "none"]
         self.run_algo_greedy = greedy
         self.run_algo_bfs = bfs
         self.run_bfs_repeat = False
@@ -24,81 +20,49 @@ class snakegame:
         self.lost = False
         self.game_begun = False
 
-        # double the size of with turtles.
         self.initTiles = tiles(36, self.width, self.height)
         self.initTiles.drawBoard()
         self.initSnake = snake()
         self.initSnake.reset(self.initTiles)
         self.initApple = apple()
         self.initApple.spawnApple(self.initTiles, self.initSnake)
-        #self.initMessage = messages()
 
         if ml == True:
             self.run_ml = True
             self.run_player = False
             self.run_algo_bfs = False
             self.run_algo_greedy = False
+        print("init complete!")
 
     def start(self):
         self.game_begun = True
         counter = 0
         while self.lost == False and self.run_ml == False:
-            # read user input.
-
+            print("updating time")
             self.updateTime()
             time.sleep(self.update_time/1000.0)
-        
-        #while self.lost == True:
-            #pygame.draw.rect(self.screen, (170, 170, 170), [self.window_width//2-15, self.height+50, 30, 30])
-            #events = pygame.event.get()
-            #for event in events:
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()
-            #    elif event.type == pygame.MOUSEBUTTONDOWN:
-            #        mouse = pygame.mouse.get_pos()
-            #        if self.window_width//2-15 <= mouse[0] <= self.window_width//2+15 and self.height+50 <= mouse[1] <= self.height + 80:
-            #            self.reset()
-            #pygame.display.update()
-            
-            #do something about user losing
         if self.lost == True:
             self.reset()
 
     def left(self):
         if self.initSnake.direction == "right":
             return
-        if self.buffer[0] == "none":
-            self.buffer[0] = "left"
-            self.initSnake.rotate("left")
-        elif self.buffer[1] == "none":
-            self.buffer[1] = "left"
+        self.initSnake.rotate("left")
     
     def down(self):
         if self.initSnake.direction == "up":
             return
-        if self.buffer[0] == "none":
-            self.buffer[0] = "down"
-            self.initSnake.rotate("down")
-        elif self.buffer[1] == "none":
-            self.buffer[1] = "down"
+        self.initSnake.rotate("down")
     
     def right(self):
         if self.initSnake.direction == "left":
             return
-        if self.buffer[0] == "none":
-            self.buffer[0] = "return"
-            self.initSnake.rotate("right")
-        elif self.buffer[1] == "none":
-            self.buffer[1] = "right"
+        self.initSnake.rotate("right")
     
     def up(self):
         if self.initSnake.direction == "down":
             return
-        if self.buffer[0] == "none":
-            self.buffer[0] = "up"
-            self.initSnake.rotate("up")
-        elif self.buffer[1] == "none":
-            self.buffer[1] = "up"
+        self.initSnake.rotate("up")
     
     def runGreedy(self):
         alg = algorithm(self.update_time, self.initTiles)
@@ -146,12 +110,10 @@ class snakegame:
 
         if self.initSnake.xcor < 0 or self.initSnake.ycor < 0 or self.initSnake.xcor >= self.initTiles.amountWidth or self.initSnake.ycor >= self.initTiles.amountHeight:
             self.lost = True
-            #self.initMessage.draw("You Lost! ", self.apple_count, self.screen, self.width, self.height)
             return
 
         if self.initSnake.direction != "none" and self.initSnake.move(self.initTiles) == False:
             self.lost = True
-            #self.initMessage.draw("You Lost! ", self.apple_count, self.screen, self.width, self.height)
             return
 
         if self.initSnake.xcor == self.initApple.xcoord and self.initSnake.ycor == self.initApple.ycoord:
@@ -162,30 +124,7 @@ class snakegame:
         for i in range(1, len(self.initSnake.snakeCoords)):
             if self.initSnake.xcor == self.initSnake.snakeCoords[i][0] and self.initSnake.ycor == self.initSnake.snakeCoords[i][1]:
                 self.lost = True
-                #self.initMessage.draw("You Lost! ", self.apple_count, self.screen, self.width, self.height)
                 return
-        
-        if self.buffer[1] != "none":
-            self.initSnake.rotate(self.buffer[1])
-        self.buffer[0] = "none"
-        self.buffer[1] = "none"
-
-        # curr_tiles = self.initTiles.returnTiles()
-        # print("\n", end=" ")
-
-        # for tile_row in curr_tiles:
-        #     for tile in tile_row:
-        #         if tile == "lime" or tile == "green":
-        #             print("o", end=" ")
-        #         elif tile == "lightblue" or tile == "blue":
-        #             print("x", end=" ")
-        #         elif tile == "red":
-        #             print("a", end=" ")
-        #         else:
-        #             print("tile = " + tile)
-        #     print("\n", end=" ")
-
-        #pygame.display.update()
     
     def reset(self):
         if self.lost == True:
@@ -197,7 +136,6 @@ class snakegame:
             self.initSnake.reset(self.initTiles)
             self.initApple.spawnApple(self.initTiles, self.initSnake)
             self.apple_count = 0
-            self.buffer = ["none", "none"]
             self.list_of_directions = []
             self.initSnake.direction = "none"
             self.run_bfs_repeat = False
